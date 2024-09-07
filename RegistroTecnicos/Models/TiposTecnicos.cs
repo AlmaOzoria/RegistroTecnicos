2ef1b0e75b77c7ca19015904bb4ff2d299b8c74c
Alma Ozoria <heisylior@hotmail.com>
2024-09-06 21:36:30 -0400

    Creando clase TiposTecnicos

--- RegistroTecnicos/Models/TiposTecnicos.cs
+++ RegistroTecnicos/Models/TiposTecnicos.cs
@@ -1,6 +1,16 @@
-﻿namespace RegistroTecnicos.Models
+﻿using System.ComponentModel.DataAnnotations;
+using RegistroTecnicos.Models;
+
+namespace RegistroTecnicos.Models;
+
+public class TiposTecnicos
 {
-    public class TiposTecnicos
-    {
-    }
+    [Key]
+    [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual  que 1 ")]
+    public int TipoTecnicoId { get; set; }
+
+    [Required(ErrorMessage = " La descripcion es obligatoria.")]
+    public string Descripcion { get; set; } = string.Empty;
+    public ICollection<Tecnicos> Tecnicos { get; set; } = new List<Tecnicos>();
+    
 }
