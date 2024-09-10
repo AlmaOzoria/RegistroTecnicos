a8b6a7df5e5d76206a2141f03fc8689daad66adc
Alma Ozoria <heisylior@hotmail.com>
2024-09-10 18:20:05 -0400

    Se la clase clientes en el Models

---
+++ RegistroTecnicos/Models/Clientes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace RegistroTecnicos.Models;
+
+public class Clientes
+{
+    [Key]
+
+    [Required(ErrorMessage = "El Campo Cliente es obligatorio")]
+    public int ClientesId { get; set; }
+
+    [Range(1, int.MaxValue, ErrorMessage = "El Campo Nombre es obligatorio")]
+    public string? Nombres { get; set; }
+    [Range(0.01, double.MaxValue, ErrorMessage = "El campo WhatsApp es obligatorio.")]
+    public string? WhatsApp { get; set; }
+    
+}
