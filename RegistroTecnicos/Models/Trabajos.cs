2e9c29ea11a4166afb1cd6cc345077a4995c9109
Alma Ozoria <heisylior@hotmail.com>
2024-09-12 20:13:52 -0400

    Creando la clase trabajos del models

---
+++ RegistroTecnicos/Models/Trabajos.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace RegistroTecnicos.Models;
+
+public class Trabajos
+{
+    [Key]
+
+    [Required(ErrorMessage = "El Campo Trabajo es obligatorio")]
+    public int TrabajosId { get; set; }
+
+    [Required(ErrorMessage = "El Campo Nombre es obligatorio")]
+    public string? Nombres { get; set; }
+
+    [Required(ErrorMessage = "El Campo Cliente es obligatorio")]
+    public int ClientesId { get; set; }
+
+    [Required(ErrorMessage = "El Campo  Descripcion es obligatorio.")]
+    public string? Descripcion { get; set; }
+
+    [Required(ErrorMessage = "El Campo Monto es obligatorio.")]
+    [Range(0, double.MaxValue, ErrorMessage = "El Monto debe ser un valor positivo.")]
+    public decimal Monto { get; set; }
+
+    [Required(ErrorMessage = "El Campo Fecha es obligatorio.")]
+    [DataType(DataType.Date)]
+    public DateTime Fecha { get; set; }
+}
