25d57e77e4562ea4551de4684b8ca2bd7d0eff1c
Alma Ozoria <heisylior@hotmail.com>
2024-09-17 23:25:11 -0400

    Creando la clase prioridades de la carperta models

---
+++ RegistroTecnicos/Models/Prioridades.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace RegistroTecnicos.Models;
+
+public class Prioridades
+{
+    [Key]
+    [Range(1, int.MaxValue, ErrorMessage = "El ID debe ser mayor o igual  que 1 ")]
+    public int PrioridadesId { get; set; }
+
+    [Required(ErrorMessage = " La descripcion es obligatoria.")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = " El tiempo es obligatoria.")]
+    public TimeSpan Tiempo { get; set; }
+
+}
