afa981efc66404c0281192fd5afb94ffff420747
Alma Ozoria <heisylior@hotmail.com>
2024-10-18 22:54:52 -0400

    Creando el modelo de trabajo detalle

---
+++ RegistroTecnicos/Models/TrabajoDetalle.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models;
+
+public class TrabajoDetalle
+{
+    [Key]
+
+
+    public int? DetalleId { get; set; }
+
+    [ForeignKey("TrabajoId")]
+    public Trabajos? TrabajoId { get; set; }
+
+
+    [ForeignKey("articuloId")]
+    public Articulos? ArticuloId { get; set; }
+
+
+    [Required(ErrorMessage = "Este Campo debe de ser obligatorio")]
+    public decimal? Cantidad { get; set; } 
+    
+    [Required(ErrorMessage = "Este Campo debe de ser obligatorio")]
+    public decimal? Precio { get; set; }
+
+    [Required(ErrorMessage = "Este Campo debe de ser obligatorio")]
+    public decimal? Costo { get; set; }
+
+   
+}
+
+
