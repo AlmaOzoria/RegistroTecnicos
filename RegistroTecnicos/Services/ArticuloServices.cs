921ea048ba16ae0633f9b52560d820db1ecec97c
Alma Ozoria <heisylior@hotmail.com>
2024-10-18 22:53:20 -0400

    Creando el articulo services

--- RegistroTecnicos/Services/ArticuloServices.cs
+++ RegistroTecnicos/Services/ArticuloServices.cs
@@ -8,71 +8,46 @@ namespace RegistroTecnicos.Services;
 public class ArticuloServices
 {
     private readonly Contexto _contexto;
+
     public ArticuloServices(Contexto contexto)
     {
         _contexto = contexto;
     }
 
-    public async Task<bool> Existe(int servicioId)
+    public async Task<List<Articulos>> ListaArticulos()
     {
-        return await _contexto.Servicio.AnyAsync(s => s.ServicioId == servicioId);
-    }
-
-    private async Task<bool> Insertar(Articulos servicio)
-    {
-        _contexto.Servicio.Add(servicio);
-        return await _contexto.SaveChangesAsync() > 0;
-    }
-
-    private async Task<bool> Modificar(Articulos servicio)
-    {
-        _contexto.Servicio.Update(servicio);
-        var modificado = await _contexto.SaveChangesAsync() > 0;
-        _contexto.Entry(servicio).State = EntityState.Detached;
-        return modificado;
-    }
-
-    public async Task<bool> Guardar(Articulos servicio)
-    {
-        if (!await Existe(servicio.ServicioId))
-            return await Insertar(servicio);
-        else
-            return await Modificar(servicio);
-    }
-
-    public async Task<bool> Eliminar(int id)
-    {
-        var EliminarServicio = await _contexto.Servicio
-            .Where(s => s.ServicioId == id)
-            .ExecuteDeleteAsync();
-        return EliminarServicio > 0;
-    }
-
-    public async Task<Articulos?> Buscar(int id)
-    {
-        return await _contexto.Servicio
+        return await _contexto.Articulos
             .AsNoTracking()
-            .FirstOrDefaultAsync(s => s.ServicioId == id);
-    }
-
-    public async Task<List<Articulos>> Listar(Expression<Func<Articulos, bool>> criterio)
-    {
-        return await _contexto.Servicio
-            .AsNoTracking()
-            .Where(criterio)
             .ToListAsync();
     }
 
-    public async Task<bool> ExiteDescripcion(string descripcion, int? servicioId = null)
+    public async Task<Articulos?> ObtenerArticuloPorId(int id)
     {
-        if (servicioId.HasValue)
+        return await _contexto.Articulos
+            .AsNoTracking()
+            .FirstOrDefaultAsync(a => a.ArticuloId == id);
+    }
+
+    public async Task ActualizarExistencia(int articuloId, decimal cantidad)
+    {
+        var articulo = await _contexto.Articulos.FindAsync(articuloId);
+        if (articulo != null)
         {
-            return await _contexto.Servicio.AnyAsync(s => s.Descripcion == descripcion && s.ServicioId != servicioId);
-        }
-        else
-        {
-            return await _contexto.Servicio.AnyAsync(s => s.Descripcion == descripcion);
+            articulo.Existencia -= cantidad; 
+            _contexto.Articulos.Update(articulo);
+            await _contexto.SaveChangesAsync();
         }
     }
 
+    public async Task AgregarCantidad(int articuloId, int cantidad)
+    {
+        var articulo = await _contexto.Articulos.FindAsync(articuloId);
+
+        if (articulo != null)
+        {
+            articulo.Existencia += cantidad;
+
+            await _contexto.SaveChangesAsync();
+        }
+    }
 }
