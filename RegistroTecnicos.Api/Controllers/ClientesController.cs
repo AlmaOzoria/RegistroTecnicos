1a96858a431dfd20aabaefe88960bb97d27a9a8b
Alma Ozoria <heisylior@hotmail.com>
2024-11-01 19:51:14 -0400

    creando clase de clientes controller

---
+++ RegistroTecnicos.Api/Controllers/ClientesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+//using RegistroTecnicos.DAL;
+
+namespace RegistroTecnicos.Api.Controllers
+{
+    //[Route("api/[controller]")]
+    //[ApiController]
+   
+    //public class ClientesController : ControllerBase
+    //{
+    //    private readonly Contexto _context;
+    //    public ClientesController(Contexto context)
+    //    {
+    //        _context = context;
+
+    //    }
+
+    //    [HttpGet]
+    //    [Route("lista")]
+    //    public async Task<ActionResult> Get()
+    //    {
+    //        var listaClientes = await _context.Clientes.ToListAsync();
+    //        return Ok(listaClientes);
+    //    }
+    //}
+
+}
